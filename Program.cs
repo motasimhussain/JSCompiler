ddc66f22584c6a02a2f6d9635528593a48b9b390
Motasim Hussain <motasimhussain@gmail.com>
2014-09-09 01:00:02 +0500

    Initial Commit
    Program is able to read a file and store individual lines in an array.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCompiler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the path of the file to be read!!");
+            string path = Console.ReadLine();
+            lexAnalyse la = new lexAnalyse(path);
+
+            string[] lineArr = la.readFile();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ lexAnalyse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JSCompiler
+{
+    class lexAnalyse
+    {
+        public string path;
+
+        public lexAnalyse(string path) {
+            this.path = path;
+        }
+
+        public string[] readFile(){
+            int counter = 0;
+            string line;
+            string[] lineArr;
+
+            try{
+                System.IO.StreamReader file = new System.IO.StreamReader(path);
+                int count = File.ReadLines(path).Count();     // Counting the number of lines in a file
+                lineArr = new string[count];
+
+                while ((line = file.ReadLine()) != null){     // Storing individual lines in an array
+                    Console.WriteLine(line);
+                    lineArr[counter] = line;
+                    counter++;
+                }
+
+                file.Close();
+                return lineArr;                               //returning an array
+
+            }catch (Exception e){
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+
+            }
+
+            return lineArr=new string[0];                     //returning empy array if file read fails!
+        }
+    }
+}
