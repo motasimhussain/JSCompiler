afcab5f795bc6910b9f93e5f631db01aa24a654b
Motasim Hussain <motasimhussain@gmail.com>
2014-09-23 14:16:10 +0500

    User Input Enabled

--- Program.cs
+++ Program.cs
@@ -15,10 +15,11 @@ namespace JSCompiler
         {
             string path = "c:/test.txt";
             string outPath = "e:/out.txt";
-            //Console.WriteLine("Enter the path of the file to be read:");
-            //string path = Console.ReadLine();
-            //Console.WriteLine("Enter the path of the output file to be created:");
-            //string outPath = Console.ReadLine();
+
+            Console.WriteLine("Enter the path of the file to be read:");
+            path = Console.ReadLine();
+            Console.WriteLine("Enter the path of the output file to be created:");
+            outPath = Console.ReadLine();
             try
             {
                 if (File.Exists(outPath))
